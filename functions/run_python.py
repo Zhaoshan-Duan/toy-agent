7673ed9278607349494098e14e0ab8cb75cf6aa8
Zhaoshan-Duan <joshua.duan47@gmail.com>
2025-08-18 17:50:26 -0700

    feat: Implement run_python_file function - Add core functionality to execute Python files - Include robust security checks (directory traversal, file existence, .py extension) - Handle process output, errors, and timeouts

---
+++ functions/run_python.py
@@ -0,0 +1,42 @@
+import os
+import subprocess
+
+def run_python_file(working_directory, file_path, args=[]):
+    working_dir = os.path.abspath(working_directory)
+    abs_file_path = os.path.abspath(os.path.join(working_directory, file_path))
+
+    if not abs_file_path.startswith(working_dir):
+        return f'Error: Cannot execute "{file_path}" as it is outside the permitted working directory'
+
+    if not os.path.exists(abs_file_path):
+        return f'Error: File "{file_path}" not found.'
+
+    if not abs_file_path.endswith(".py"):
+        return f'Error: "{file_path}" is not a Python file.'
+
+    try:
+        command = ["python", abs_file_path]
+        if args:
+            command.extend(args)
+        result = subprocess.run(
+            args = command,
+            capture_output=True,
+            timeout=30,
+            text=True,
+            cwd=working_dir,
+        )
+
+        output = []
+        if result.stdout:
+            output.append(f"STDOUT: \n{result.stdout}")
+        if result.stderr:
+            output.append(f"STDERR: \n{result.stderr}")
+
+        if result.returncode != 0:
+            output.append(f"Process exited with code {result.returncode}")
+
+        return "\n".join(output) if output else "No output."
+
+    except Exception as e:
+        return f"Error: executing Python file: {e}"
+
